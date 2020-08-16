5438a8ec9582492d5cd121e960be7797af6ab184
0xBono <iam@Sonjim.in>
2020-08-17 00:53:18 +0900

    feat(common): added common string setting

---
+++ src/common/constant/string.ts
@@ -0,0 +1,5 @@
+export const DEFAULT_MAIN_TEXT = '투데이';
+export const HOT_PICK = '핫픽!';
+export const NEARBY_RESTAURANTS = '당신을 위한 주변 맛집';
+export const FIND_MAP = '지도로 찾아보세요!';
+export const YOU_LIKE_RANDOM = '돌려돌려 돌림판';
