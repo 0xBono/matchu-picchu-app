0d1a9704029b87fd9cca20543b6bbe188dd7160f
0xBono <iam@Sonjim.in>
2020-08-17 01:00:14 +0900

    feat(common): added common color setting

---
+++ src/common/constant/color.ts
@@ -0,0 +1,6 @@
+export const WHITE_COLOR = '#FFFFFF';
+export const BLACK_COLOR = '#1B1C1D';
+export const GRAY_COLOR = '#858A8D';
+export const GRAY_BACKGROUND_COLOR = '#F8F8F9';
+export const ORANGE_COLOR = '#F9A826';
+export const RED_COLOR = '#F33340';
